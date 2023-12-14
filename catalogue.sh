99077112dd20aa4dd09c4ecad3a3b23d0a2ecd0a
App <srinethsri@gmail.com>
2023-12-14 13:50:01 +0530

    sri

--- catalogue.sh
+++ catalogue.sh
@@ -1,23 +1,23 @@
-echo -e "\e[36m downloding nodejs repo \e[om"
+echo -e "\e[31m downloding nodejs repo \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[36m installing nodejs \e[om"
+echo -e "\e[31m installing nodejs \e[0m"
 yum install nodejs -y
-echo -e "\e[36m adding user and path \e[om"
+echo -e "\e[31m adding user and path \e[0m"
 useradd roboshop
 mkdir /app
 cd /app
-echo -e "\e[36m downloading new app content and there dependencies \e[om"
+echo -e "\e[31m downloading new app content and there dependencies \e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 unzip catalogue.zip
 rm -rf catalogue.zip
 npm install
-echo -e "\e[36m creating catalogue service file \e[om"
+echo -e "\e[36m creating catalogue service file \e[0m"
 cp /root/roboshop/catalogue.service /etc/systemd/system/catalogue.service
-echo -e "\e[36m downloading and loading the mongodb schema \e[om"
+echo -e "\e[31m downloading and loading the mongodb schema \e[0m"
 cp /root/roboshop/mongodb.repos  /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host mongodb.srineeth.tech < /app/schema/catalogue.js
-echo -e "\e[36m enabling and restaring the catalogue service \e[om"
+echo -e "\e[31m enabling and restaring the catalogue service \e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
