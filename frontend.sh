9db8c431fa7cd23b6616a00af629221f3ef2d8a4
App <srinethsri@gmail.com>
2023-12-20 13:09:33 +0530

    sri

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,13 @@
-echo -e "\e[36m installing nginx service \[eom"
+echo -e "\e[36m installing nginx service \e[om"
 yum install nginx -y
-echo -e "\e[36m removing default server content \[eom"
+echo -e "\e[36m removing default server content \e[om"
 cd /usr/share/nginx/html/
 rm -rf *
-echo -e "\e[36m downloding custom server content \[eom"
+echo -e "\e[36m downloding custom server content \e[om"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip frontend.zip
 rm -rf fontend.zip
 cp /root/roboshop/roboshop.conf /etc/nginx/default.d/roboshop.conf
-echo -e "\e[36m enabling and restarting the nginx service \[eom"
+echo -e "\e[36m enabling and restarting the nginx service \e[om"
 systemctl enable nginx
 systemctl restart nginx
