cf9bf2eda30a0187d502ffc05c5848324a68e149
App <srinethsri@gmail.com>
2023-12-20 13:32:00 +0530

    sri

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,6 @@ echo -e "\e[36m downloding custom server content \e[om"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip frontend.zip
 rm -rf fontend.zip
-cp /root/roboshop/roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[36m enabling and restarting the nginx service \e[om"
 systemctl enable nginx
 systemctl restart nginx
